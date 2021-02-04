056b216dba434557e6e552cb5ffd52d9489dc364
Stephan Lange <stephan.lange@awi.de>
2021-02-04 14:49:27 +0100

    Upload New File

---
+++ additionals_shiny/appCSS.R
@@ -0,0 +1,35 @@
+# CSS modifications
+# old colors: body & th background color #003E6E
+# well colors: #07ACE7
+# layout
+# .irs ==> for slider
+appCSS <- "
+th {
+padding-top: 12px;
+padding-bottom: 12px;
+text-align: left;
+background-color: #BDB5B9;
+color: black;
+}
+body {
+background-color: #A6B896;
+}
+.well {
+background-color: #BBCEB2;
+border-color: #BBCEB2;
+border-radius: 1px;
+}
+.btn-loading-container {
+margin-left: 10px;
+font-size: 1.2em;
+}
+.btn-done-indicator {
+color: green;
+}
+.btn-err {
+margin-top: 10px;
+color: red;
+}
+.col-sm-4 {width: 250px;}
+.irs-from, .irs-to, .irs-single, .irs-grid-text { color: #000000 }
+"
